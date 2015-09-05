5f143ccc002f37c9b542c0856b291576b6698ce9
teryaew <mitya.teryaew@gmail.com>
2015-09-05 13:59:57 +0300

    0.1.0: initial release

---
+++ README.md
@@ -0,0 +1,35 @@
+# enb-favicons
+
+[ENB](https://github.com/enb-make/enb) technology for favicons generation. Wrapper around [favicons](https://github.com/haydenbleasel/favicons) package.
+
+## Installing
+
+```
+npm install --save-dev enb-favicons
+```
+
+## Options
+Options are the same as in `favicons` package, except `target` (for enb tech process).
+
+* *Object* **files** — Paths to files. There are required fields: `src` & `dest`.
+* *Object* **icons** — Icon types.
+* *Object* **settings** — Additional settings.
+* *Object* **favicon_generation** — Complete JSON overwrite for the favicon_generation object.
+* *String* **target** — Target for technology. Required option.
+
+## Example
+
+```javascript
+config.node('src/blocks/common.blocks/favicons', function(nodeConfig) {
+    nodeConfig.addTech(require('enb/techs/enb-favicons'), {
+        files : {
+            src : 'public/favicons/src/favicon.png',
+            dest : 'public/favicons/dest/',
+            html : 'public/favicons/dest/favicons.html',
+            iconsPath : '/favicons/dest/'
+        },
+        target : '?.bemhtml'
+    });
+    nodeConfig.addTargets([ '?.bemhtml' ]);
+});
+```
---
+++ techs/enb-favicons.js
@@ -0,0 +1,114 @@
+/**
+ * enb-favicons
+ * ============
+ *
+ * ENB technology for favicons generation. Wrapper around [favicons](https://github.com/haydenbleasel/favicons) package.
+ * Requirements for favicons source (master) picture:
+ * 1. Should be square
+ * 2. Should be at least 57x57 (for ios), 70x70 (windows), 228x228 (coast), 300x300 (open_graph).
+ *
+ * **Options**
+ * Options are the same as in `favicons` package, except `target` (for enb tech process).
+ *
+ * * *Object* **files** — Paths to files. There are required fields: `src` & `dest`.
+ * * *Object* **icons** — Icon types.
+ * * *Object* **settings** — Additional settings.
+ * * *Object* **favicon_generation** — Complete JSON overwrite for the favicon_generation object.
+ * * *String* **target** — Target for technology. Required option.
+ *
+ * **Example**
+ *
+ * ```javascript
+ * config.node('src/blocks/common.blocks/favicons', function(nodeConfig) {
+ *     nodeConfig.addTech(require('enb/techs/enb-favicons'), {
+ *         files : {
+ *             src : 'public/favicons/src/favicon.png',
+ *             dest : 'public/favicons/dest/',
+ *             html : 'public/favicons/dest/favicons.html',
+ *             iconsPath : '/favicons/dest/'
+ *         },
+ *         target : '?.bemhtml'
+ *     });
+ *     nodeConfig.addTargets([ '?.bemhtml' ]);
+ * });
+ * ```
+ */
+
+var favicons = require('favicons');
+var html2bemjson = require('html2bemjson');
+var _ = require('lodash');
+var vow = require('vow');
+
+module.exports = require('enb/lib/build-flow').create()
+    .name('enb-favicons')
+    .target('target')
+
+    .builder(function() {
+        var _this = this;
+        var def = vow.defer();
+        var options = _.defaultsDeep(this._options || {}, {
+            files : {
+                src : null,
+                dest : null,
+                html : null,
+                iconsPath : null,
+                androidManifest : null,
+                browserConfig : null,
+                firefoxManifest : null,
+                yandexManifest : null
+            },
+            icons : {
+                android : true,
+                appleIcon : true,
+                appleStartup : true,
+                coast : true,
+                favicons : true,
+                firefox : true,
+                opengraph : true,
+                windows : true,
+                yandex : true
+            },
+            settings : {
+                appName : null,
+                appDescription : null,
+                developer : null,
+                developerURL : null,
+                version : 1.0,
+                background : null,
+                index : null,
+                url : null,
+                silhouette : false,
+                logging : true
+            },
+            favicon_generation : null,
+            target : null
+        });
+
+
+        if (options.files.html) {
+            favicons(options, function (error, metadata) {
+                if (error) {
+                    console.error('Error: ' + error.favicon_generation_result.result.error_message);
+                    def.reject();
+                } else {
+                    def.resolve(_this.getTpl(metadata));
+                }
+            });
+        } else {
+            favicons(options, function (metadata) {
+                def.resolve(_this.getTpl(metadata));
+            });
+        }
+
+        return def.promise();
+    })
+    .methods({
+        getTpl : function(metadata) {
+            return [
+                'block(\'favicons\').def()(function() {',
+                'applyCtx(' + html2bemjson.stringify(metadata) + ');',
+                '})'
+            ].join('\n');
+        }
+    })
+    .createTech();
